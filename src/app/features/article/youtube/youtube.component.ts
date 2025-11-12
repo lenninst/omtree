70a6cfab09e33bcd9a29ec70941ef98e738a0424
lenninst <leninguaminga@gmail.com>
2025-11-12 11:26:07 -0500

    feat: agregado terminal component

--- src/app/features/article/youtube/youtube.component.ts
+++ src/app/features/article/youtube/youtube.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { OmterminalComponent } from '../../components/omterminal/omterminal.component';
 
 @Component({
   selector: 'app-youtube',
-  imports: [],
+  imports: [OmterminalComponent],
   templateUrl: './youtube.component.html',
-  styleUrl: './youtube.component.css'
+  styleUrl: './youtube.component.css',
 })
-export class YoutubeComponent {
-
-}
+export class YoutubeComponent {}
---
+++ src/app/features/components/omterminal/omterminal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-omterminal',
+  // imports: [FormsModule],
+  templateUrl: './omterminal.component.html',
+  styleUrl: './omterminal.component.css',
+})
+export class OmterminalComponent {
+  command = '';
+}
