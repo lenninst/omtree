f63e86a949776bc27c4b506b143c2c5aab543a7f
lenninst <leninguaminga@gmail.com>
2025-11-17 13:47:18 -0500

    feat: terminal emulator ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './core/layout/footer/footer.component';
 import { HeaderComponent } from './core/layout/header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [FormsModule, RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'omtree';
--- src/app/features/components/omterminal/omterminal.component.ts
+++ src/app/features/components/omterminal/omterminal.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-omterminal',
-  // imports: [FormsModule],
+  imports: [FormsModule],
   templateUrl: './omterminal.component.html',
   styleUrl: './omterminal.component.css',
 })
 export class OmterminalComponent {
-  command = '';
+  log = 'Mi terminal';
+  command: string = '';
+
+  executeCommand(): void {
+    this.log += `\n$ ${this.command}`;
+    if (this.command === 'help') {
+      this.log += '\n Available commands: help, echo, clear';
+    } else if (this.command === 'clear') {
+      this.log = '';
+    } else {
+      this.log += `\m Command not found: ${this.command}`;
+    }
+    this.command = '';
+  }
 }
