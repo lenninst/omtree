22687290878e66bd46cd3d0e98beb6dae1c8cb84
lenninst <leninguaminga@gmail.com>
2025-11-18 13:25:06 -0500

    feat: no se que hice pero aqui lo dejo xd

--- src/app/features/components/omterminal/omterminal.component.ts
+++ src/app/features/components/omterminal/omterminal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -8,18 +8,52 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './omterminal.component.css',
 })
 export class OmterminalComponent {
+  @ViewChild(`scrollContainer`) private scrollContainer!: ElementRef;
   log = 'Mi terminal';
   command: string = '';
 
   executeCommand(): void {
-    this.log += `\n$ ${this.command}`;
-    if (this.command === 'help') {
-      this.log += '\n Available commands: help, echo, clear';
-    } else if (this.command === 'clear') {
-      this.log = '';
-    } else {
-      this.log += `\m Command not found: ${this.command}`;
+    // this.log += `\n ${this.command}`;
+    // if (this.command === 'help') {
+    //   this.log += '\n Available commands:youtube, github, spotify, help, clear';
+    // } else if (this.command === 'clear') {
+    //   this.log = '';
+    // } else {
+    //   this.log += `\n Command not found: ${this.command}`;
+    // }
+    // this.command = '';
+    //
+    switch (this.command) {
+      case 'youtube':
+        this.youtubeScrapper();
+        break;
+      case 'github':
+        console.log('Navigating to GitHub...');
+        break;
+      case 'spotify':
+        console.log('Navigating to Spotify...');
+        break;
+      case 'help':
+        this.log += '\n Available commands: youtube, github, spotify, help, clear';
+        break;
+      case 'clear':
+        this.log = '';
+        break;
+      default:
+        console.log(`Command not found: ${this.command}`);
+        break;
     }
-    this.command = '';
+  }
+
+  youtubeScrapper(): void {
+    console.log('YouTube scrapper function called');
+  }
+  ngAterViewChecked(): void {
+    this.scrollToBottom();
+  }
+
+  private scrollToBottom(): void {
+    const el = this.scrollContainer.nativeElement;
+    el.scrollTop = el.scrollHeight;
   }
 }
